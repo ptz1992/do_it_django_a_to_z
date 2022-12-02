e646e782945c92a610804ebb2074e8ad2972a1ad
park <ptz1992@naver.com>
2022-12-02 14:44:26 +0900

    태그 기능 추가하기

--- blog/admin.py
+++ blog/admin.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
-from .models import Post, Category
+from .models import Post, Category, Tag
 
 admin.site.register(Post)
 
 # 슬러그 생성을 위한 클래스 추가
-class CategoryAdmin(admin.ModelAdmin) :
-    prepopulated_fields = { 'slug': ('name', )}
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name', )}
 
-admin.site.register(Category)
+class TagAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name', )}
+
+admin.site.register(Category,CategoryAdmin)
+admin.site.register(Tag, TagAdmin)
--- blog/models.py
+++ blog/models.py
@@ -2,18 +2,30 @@ from django.db import models
 from django.contrib.auth.models import User
 import os
 
-class Category(models.Model) :
+class Category(models.Model):
     name = models.CharField(max_length=50, unique=True)
-
     # slug : url을 생성하기 위해 문자를 조합하는 방식
-    slug = models.SlugField(max_length=50, unique=True, allow_unicode=True)
+    slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
 
     def __str__(self):
         return self.name
 
+    def get_absolute_url(self):
+        return f'/blog/category/{self.slug}/'
+
     class Meta:
         verbose_name_plural = 'Categories'
 
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    # slug : url을 생성하기 위해 문자를 조합하는 방식
+    slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return f'/blog/category/{self.slug}/'
 
 class Post(models.Model):
     title = models.CharField(max_length=30)
@@ -29,6 +41,7 @@ class Post(models.Model):
 
     category = models.ForeignKey(Category, null=True, blank=True,
                                  on_delete=models.SET_NULL)
+    tags = models.ManyToManyField(Tag, blank=True)
 
     def __str__(self):
         return f'[{self.pk}] {self.title} :: {self.author}'
@@ -41,4 +54,7 @@ class Post(models.Model):
 
     def get_file_ext(self):
         return self.get_file_name().split('.')[-1]
+
+
+
 
