cd8c99ee8f7cebc579c6362ff818b3a59372e8dd
park <ptz1992@naver.com>
2022-12-05 15:29:09 +0900

    Content 입력란에 마크다운 적용하기

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
+from markdownx.admin import MarkdownxModelAdmin
 from .models import Post, Category, Tag
 
-admin.site.register(Post)
+admin.site.register(Post, MarkdownxModelAdmin)
 
 # 슬러그 생성을 위한 클래스 추가
 class CategoryAdmin(admin.ModelAdmin):
