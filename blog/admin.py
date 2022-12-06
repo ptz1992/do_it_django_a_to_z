67f523e67345c271d2be99d39799da1496b438fb
park <ptz1992@naver.com>
2022-12-06 09:50:01 +0900

    댓글을 상세 페이지에 나타내기

--- blog/admin.py
+++ blog/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from markdownx.admin import MarkdownxModelAdmin
-from .models import Post, Category, Tag
+from .models import Post, Category, Tag, Comment
 
 admin.site.register(Post, MarkdownxModelAdmin)
+admin.site.register(Comment)
 
 # 슬러그 생성을 위한 클래스 추가
 class CategoryAdmin(admin.ModelAdmin):
